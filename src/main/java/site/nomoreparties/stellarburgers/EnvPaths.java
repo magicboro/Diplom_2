137d4c9c52641a0088a3bb4bbb873c951098d5d9
Igor Borozdin <igor@borozd.in>
2024-10-24 01:34:37 +0300

    Add api paths class

---
+++ src/main/java/site/nomoreparties/stellarburgers/EnvPaths.java
@@ -0,0 +1,12 @@
+package site.nomoreparties.stellarburgers;
+
+public class EnvPaths {
+
+    public static final String BASE_URI = "https://stellarburgers.nomoreparties.site";
+    public static final String API_PREFIX = "/api";
+    public static final String USER_AUTH_PATH = API_PREFIX + "/auth";
+    public static final String USER_REG_PATH = USER_AUTH_PATH + "/register";
+    public static final String USER_LOGIN_PATH = USER_AUTH_PATH + "/register";
+    public static final String USER_PATH = USER_AUTH_PATH + "/user";
+    public static final String ORDERS_PATH = API_PREFIX + "/orders";
+}
