f623814111988aec2334d87485b307c1e06c62bb
Igor Borozdin <igor@borozd.in>
2024-10-24 23:56:17 +0300

    Fix login path

--- src/main/java/site/nomoreparties/stellarburgers/EnvPaths.java
+++ src/main/java/site/nomoreparties/stellarburgers/EnvPaths.java
@@ -6,7 +6,7 @@ public class EnvPaths {
     public static final String API_PREFIX = "/api";
     public static final String USER_AUTH_PATH = API_PREFIX + "/auth";
     public static final String USER_REG_PATH = USER_AUTH_PATH + "/register";
-    public static final String USER_LOGIN_PATH = USER_AUTH_PATH + "/register";
+    public static final String USER_LOGIN_PATH = USER_AUTH_PATH + "/login";
     public static final String USER_PATH = USER_AUTH_PATH + "/user";
     public static final String ORDERS_PATH = API_PREFIX + "/orders";
 }
